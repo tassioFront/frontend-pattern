e666df83f6e7c93620cdfb284b17df2b0de58fcb
Tássio <tassio72@hotmail.com>
2022-10-20 17:34:22 -0300

    chore: add tests config at craco file

--- craco.config.ts
+++ craco.config.ts
@@ -1,5 +1,28 @@
 import path from 'path';
 module.exports = {
+  jest: {
+    configure: {
+      preset: 'ts-jest',
+      modulePaths: ['<rootDir>/'],
+      rootDir: './',
+      globals: {
+        'ts-jest': {
+          tsconfig: '<rootDir>/tsconfig.json',
+        },
+      },
+      transform: {
+        '^.+\\.[t|j]sx?$': 'ts-jest',
+      },
+      setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+      moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+      moduleNameMapper: {
+        '^@/(.+)$': '<rootDir>/src/$1',
+        // workaround for axios 1.X.X version
+        // see this issue: https://github.com/axios/axios/issues/5026
+        '^axios$': require.resolve('axios'),
+      },
+    },
+  },
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
