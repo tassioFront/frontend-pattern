7c7675cd219f802440ebfe7aa3e9f002c89da2b4
Tássio <tassio72@hotmail.com>
2022-12-20 16:50:19 -0300

    chore: remove unused validation

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -3,7 +3,7 @@ import Styles from './styles';
 interface SectionTypes {
   children: JSX.Element | JSX.Element[];
   className?: string;
-  heading2?: string;
+  heading2: string;
   description?: string;
 }
 const Section = ({
@@ -15,12 +15,10 @@ const Section = ({
 }: SectionTypes): JSX.Element => {
   return (
     <Styles.Wrapper className={className} {...rest}>
-      {heading2 !== undefined && (
-        <Styles.Header>
-          <h2>{heading2}</h2>
-          {description !== undefined && <p>{description}</p>}
-        </Styles.Header>
-      )}
+      <Styles.Header>
+        <h2>{heading2}</h2>
+        {description !== undefined && <p>{description}</p>}
+      </Styles.Header>
       {children}
     </Styles.Wrapper>
   );
