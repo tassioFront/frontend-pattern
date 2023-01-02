eecd5c26b85e83554e758da94faf6efcfb1280a5
Tássio <tassio72@hotmail.com>
2023-01-02 18:03:00 -0300

    chore: add heading2 validation

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -3,7 +3,7 @@ import Styles from './styles';
 interface SectionTypes {
   children: JSX.Element | JSX.Element[];
   className?: string;
-  heading2: string;
+  heading2?: string;
   description?: string;
 }
 const Section = ({
@@ -15,10 +15,12 @@ const Section = ({
 }: SectionTypes): JSX.Element => {
   return (
     <Styles.Wrapper className={className} {...rest}>
-      <Styles.Header>
-        <h2>{heading2}</h2>
-        {description !== undefined && <p>{description}</p>}
-      </Styles.Header>
+      {heading2 !== undefined && (
+        <Styles.Header>
+          <h2>{heading2}</h2>
+          {description !== undefined && <p>{description}</p>}
+        </Styles.Header>
+      )}
       {children}
     </Styles.Wrapper>
   );
