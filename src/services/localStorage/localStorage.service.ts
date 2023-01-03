c635fc676fb7b779062692aaf01d83e979e03d70
Tássio <tassio72@hotmail.com>
2023-01-03 15:48:04 -0300

    feat: create local storage service

---
+++ src/services/localStorage/localStorage.service.ts
@@ -0,0 +1,16 @@
+export const storageService = {
+  get: <T>(key: string): T | string => {
+    const rawData = localStorage.getItem(key) as string;
+
+    try {
+      return JSON.parse(rawData) as T;
+    } catch (_) {
+      return rawData;
+    }
+  },
+  set: <T>(key: string, data: T): void => {
+    const stringifyData = JSON.stringify(data);
+
+    localStorage.setItem(key, stringifyData);
+  },
+};
