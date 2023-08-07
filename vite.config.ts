aa2bf43a4ac42e2fb02319fcaa2c5b0ea4caada2
Tássio <tassio72@hotmail.com>
2023-08-07 09:20:39 -0300

    article(vendor-size): create exclusive chunks to reduce vendor chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vitest/config';
+import { splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'node:url';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+const VISUALIZER = true; // change to see the current bundle.
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    /**
+     * See more here: https://vitejs.dev/guide/build.html#chunking-strategy
+     */
+    splitVendorChunkPlugin(),
+    /**
+     * See more here: https://www.npmjs.com/package/rollup-plugin-visualizer
+     */
+    // @ts-expect-error
+    VISUALIZER &&
+      visualizer({
+        open: true,
+        gzipSize: true,
+        filename: 'chunks-report.html',
+      }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
@@ -17,6 +37,24 @@ export default defineConfig({
   build: {
     sourcemap: true,
     outDir: './build',
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          // creating a chunk to @open-ish deps. Reducing the vendor chunk size
+          if (id.includes('@open-ish') || id.includes('tslib')) {
+            return '@open-ish';
+          }
+          // creating a chunk to react routes deps. Reducing the vendor chunk size
+          if (
+            id.includes('react-router-dom') ||
+            id.includes('@remix-run') ||
+            id.includes('react-router')
+          ) {
+            return '@react-router';
+          }
+        },
+      },
+    },
   },
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
