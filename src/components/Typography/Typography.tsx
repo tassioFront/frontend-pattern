e4d7e39e9a3a5bbf3384446f5f5e8abbf7c589f5
Tássio <tassio72@hotmail.com>
2023-08-08 21:10:31 -0300

    chore: comment typography

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,7 +1,7 @@
 import { goToId } from '@/helpers/goTo';
 import { useEffect } from 'react';
-import Styles from './styles';
-import { setIdOnUrl } from '@/helpers/setIdOnUrl';
+// import Styles from './styles';
+// import { setIdOnUrl } from '@/helpers/setIdOnUrl';
 
 interface TypographyTypes {
   label: string;
@@ -10,19 +10,18 @@ interface TypographyTypes {
 }
 
 export const component = ({ label, tag, id }: TypographyTypes): string =>
-  `<i class="fa fa-link" aria-hidden="true"></i>
-  <${tag} id="${id}">${label}</${tag}>`;
+  `<${tag} id="${id}">${label}</${tag}>`;
 
 const Typography = ({ label, id, tag }: TypographyTypes): JSX.Element => {
   useEffect(() => {
     goToId(window.location.hash);
   });
   return (
-    <Styles.Wrapper
+    <div
       dangerouslySetInnerHTML={{
         __html: component({ tag, label, id }),
       }}
-      onClick={() => setIdOnUrl(id)}
+      // onClick={() => setIdOnUrl(id)}
     />
   );
 };
