33e0948b31a2bd5a7f730e150ff1784fcbebb75a
Tássio <tassio72@hotmail.com>
2023-08-10 18:56:57 -0300

    chore(typography): remove comment code about goto id section

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,8 +1,3 @@
-import { goToId } from '@/helpers/goTo';
-import { useEffect } from 'react';
-// import Styles from './styles';
-// import { setIdOnUrl } from '@/helpers/setIdOnUrl';
-
 interface TypographyTypes {
   label: string;
   tag: 'h1' | 'h2' | 'h3';
@@ -13,15 +8,11 @@ export const component = ({ label, tag, id }: TypographyTypes): string =>
   `<${tag} id="${id}">${label}</${tag}>`;
 
 const Typography = ({ label, id, tag }: TypographyTypes): JSX.Element => {
-  useEffect(() => {
-    goToId(window.location.hash);
-  });
   return (
     <div
       dangerouslySetInnerHTML={{
         __html: component({ tag, label, id }),
       }}
-      // onClick={() => setIdOnUrl(id)}
     />
   );
 };
