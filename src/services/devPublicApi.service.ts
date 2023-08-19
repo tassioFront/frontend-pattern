072ff761b2b2fe245fca80b81add1acf8f94ee56
Tássio <tassio72@hotmail.com>
2023-08-19 20:23:51 -0300

    chore: change dev api headers and query

--- src/services/devPublicApi.service.ts
+++ src/services/devPublicApi.service.ts
@@ -4,6 +4,15 @@ import axios, { AxiosResponse } from 'axios';
 export const getOwnerDevArticlesByUserName = async (): Promise<
   AxiosResponse<IArticle[], any>
 > => {
-  return await axios.get(`https://dev.to/api/articles?username=tassiofront
-  `);
+  // more here: https://developers.forem.com/api/v1#tag/articles/operation/getArticles
+  return await axios.get(
+    `https://dev.to/api/articles?username=tassiofront&state=all 
+    
+  `,
+    {
+      headers: {
+        accept: 'application/vnd.forem.api-v1+json',
+      },
+    }
+  );
 };
