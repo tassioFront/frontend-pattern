6b9009cd3129caedbe2a2750f6a0ade42e1d0647
Tássio <tassio72@hotmail.com>
2023-09-16 18:44:51 -0300

    chore: use short cut

--- src/views/Default/Articles/components/ArticleCard/styles.ts
+++ src/views/Default/Articles/components/ArticleCard/styles.ts
@@ -7,7 +7,6 @@ const Styles = {
   Wrapper: styled(Card)`
     width: 100%;
     max-width: 300px;
-    justify-self: center;
 
     ${media.greaterThan('tablet')`
       min-height: 500px;
--- src/views/Default/Articles/components/Content/styles.ts
+++ src/views/Default/Articles/components/Content/styles.ts
@@ -4,19 +4,20 @@ import styled from 'styled-components';
 export default {
   Content: styled.div`
     display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr;
+    grid-template-columns: auto;
+    grid-auto-rows: auto;
     width: 100%;
     gap: var(--spacing-large);
+    justify-items: center;
 
     ${media.greaterThan('tablet')`
       gap: var(--spacing-medium);
-      grid-template-columns: 1fr 1fr;
+      grid-template-columns: repeat(2, 1fr);
     `}
 
     ${media.greaterThan('desktop')`
       gap: var(--spacing-medium);
-      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-columns: repeat(3, 1fr);
     `}
   `,
 };
