dbd81b7802a7554899fdfccc6b45112d10d2f584
Tássio <tassio72@hotmail.com>
2024-01-09 14:33:48 -0300

    feat: add pencil to title

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,15 +1,26 @@
-interface TypographyTypes {
+export interface TypographyTypes {
   label: string;
   tag: 'h1' | 'h2' | 'h3';
   id: string;
+  className?: string;
 }
 
-export const component = ({ label, tag, id }: TypographyTypes): string =>
-  `<${tag} id="${id}">${label}</${tag}>`;
+export const component = ({
+  label,
+  tag,
+  id,
+}: Omit<TypographyTypes, 'className'>): string =>
+  `<${tag} id="${id}" title=${label}>${label}</${tag}>`;
 
-const Typography = ({ label, id, tag }: TypographyTypes): JSX.Element => {
+const Typography = ({
+  label,
+  id,
+  tag,
+  className,
+}: TypographyTypes): JSX.Element => {
   return (
     <div
+      className={className}
       dangerouslySetInnerHTML={{
         __html: component({ tag, label, id }),
       }}
