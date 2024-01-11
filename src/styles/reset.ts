2c4b4a9ee2c3eae7fe1456f772767f21057505f1
Tássio <tassio72@hotmail.com>
2024-01-11 20:35:03 -0300

    feat: create custom scroll bar

--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -68,4 +68,41 @@ export const reset = `
         width: 100%;
         opacity: 0.15;
     }
+
+    /* Firefox */
+    * {
+      scrollbar-width: thin;
+      scrollbar-color: var(--color-neutral-light-2) var(--color-neutral-dark-1);
+    }
+
+    /* Chrome, Edge and Safari */
+    *::-webkit-scrollbar {
+      height: var(--spacing-xsmall);
+      width: var(--spacing-xsmall);
+    }
+    *::-webkit-scrollbar-track {
+      border-radius: var(--spacing-xxsmall);
+      background-color: var(--color-neutral-dark-1);
+    }
+
+    *::-webkit-scrollbar-track:hover {
+      background-color: var(--color-neutral-dark-1);
+    }
+
+    *::-webkit-scrollbar-track:active {
+      background-color: var(--color-neutral-dark-1);
+    }
+
+    *::-webkit-scrollbar-thumb {
+      border-radius: var(--spacing-xsmall);
+      background-color: var(--color-neutral-light-2);
+    }
+
+    *::-webkit-scrollbar-thumb:hover {
+      background-color: var(--color-neutral-light-2);
+    }
+
+    *::-webkit-scrollbar-thumb:active {
+      background-color: var(--color-neutral-light-3);
+    }
 `;
